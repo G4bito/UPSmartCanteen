5771394151dcc4c24bd5d5628b940afea88d82bc
codingwithalbert <icojonrodalbert@gmail.com>
2026-02-12 01:54:15 +0800

    Implement search functionality in StallFragment
    Added search functionality to filter stores in the RecyclerView.

--- app/src/main/java/com/yutahnahsyah/upsmartcanteenfrontend/fragments/StallFragment.kt
+++ app/src/main/java/com/yutahnahsyah/upsmartcanteenfrontend/fragments/StallFragment.kt
@@ -1,9 +1,12 @@
 package com.yutahnahsyah.upsmartcanteenfrontend.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,35 +17,49 @@ import com.yutahnahsyah.upsmartcanteenfrontend.adapter.StoreAdapter
 
 class StallFragment : Fragment() {
 
-  private lateinit var adapter: StoreAdapter
+    private lateinit var adapter: StoreAdapter
+    private lateinit var searchBar: EditText
 
-  private val storeList = listOf(
-    Store("Store Number One", "Category One", R.drawable.store_image),
-    Store("Store Number Two", "Category Two", R.drawable.store_image),
-    Store("Store Number Three", "Category Three", R.drawable.store_image)
-  )
+    private val storeList = listOf(
+        Store("Store Number One", "Category One", R.drawable.store_image),
+        Store("Store Number Two", "Category Two", R.drawable.store_image),
+        Store("Store Number Three", "Category Three", R.drawable.store_image)
+    )
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_stall, container, false)
-  }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-    val rv = view.findViewById<RecyclerView>(R.id.recyclerView)
-
-    adapter = StoreAdapter(storeList) { selectedStore ->
-      val bundle = Bundle().apply {
-        putString("storeName", selectedStore.name)
-      }
-      findNavController().navigate(R.id.action_nav_stall_to_nav_store_food, bundle)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_stall, container, false)
     }
 
-    rv.layoutManager = LinearLayoutManager(requireContext())
-    rv.adapter = adapter
-  }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rv = view.findViewById<RecyclerView>(R.id.recyclerView)
+        searchBar = view.findViewById(R.id.searchBar)
+
+        // Setup adapter
+        adapter = StoreAdapter(storeList) { selectedStore ->
+            val bundle = Bundle().apply {
+                putString("storeName", selectedStore.name)
+            }
+            findNavController().navigate(R.id.action_nav_stall_to_nav_store_food, bundle)
+        }
+
+        rv.layoutManager = LinearLayoutManager(requireContext())
+        rv.adapter = adapter
+
+        // Setup search functionality
+        searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapter.filter(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
 }
